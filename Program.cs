075b66d9c83c9772059066513322ef2ed916ff7c
Queiroz <kalifor_dbel@hotmail.com>
2022-03-30 14:07:08 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+using ProjectDapperBlog.Models;
+using System;
+
+namespace ProjectDapperBlog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string CONNECTION_STRING = @"Server=DESKTOP-6OMQR12\SQLEXPRESS;Database=Blog;Integrated Security=True;TrustServerCertificate=True;";
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                ReadUsers(connection);
+            }
+        }
+
+        public static void ReadUsers(SqlConnection connection)
+        {
+            var users = connection.GetAll<User>();
+            foreach (var user in users)
+            {
+                Console.WriteLine(user.Name);
+            }
+            Console.ReadKey();
+        }
+    }
+}
